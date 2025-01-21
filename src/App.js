bc3266f7befa01f723dfdb5abb8f84ebd1f939dd
Florentine Ramboeck <florentine.ramboeck@web.de>
2025-01-21 14:13:16 +0100

    created random color generator

--- src/App.js
+++ src/App.js
@@ -2,28 +2,44 @@ import randomColor from 'randomcolor';
 import { useState } from 'react';
 
 export default function ColorGenerator() {
-  const initialColor = randomColor(); // Generate the initial random color
-  console.log(initialColor);
-  const [color, setColor] = useState(initialColor.color);
+  const [color, setColor] = useState(randomColor()); // State for random color
   const generateNewColor = () => {
     const newColor = randomColor(); // Generate a new random color
     setColor(newColor); // Update the state
   };
   return (
-    <div className="Color-generator">
+    <div
+      className="Color-generator"
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
       <h1>Random Color Generator</h1>
-      <h2>Generate a random color by clicking the "Generate" button below.</h2>
+      <h3>Generate a random color by clicking the "Generate" button below.</h3>
+      <br />
       <div
         style={{
           backgroundColor: color,
-          width: '200px',
-          height: '200px',
+          width: '300px',
+          height: '300px',
           display: 'flex',
+          padding: '20px',
+          border: 'solid 2px #000000',
           justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: '20px',
+          fontWeight: 'bold',
+          transition: 'background-color 1s',
         }}
       >
         {`Generated Color: ${color}`}
       </div>
+      <br />
       <div>
         <button onClick={generateNewColor}> Generate </button>
       </div>
